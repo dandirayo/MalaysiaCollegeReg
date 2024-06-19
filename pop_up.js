8d276bbc1cfed693bd6edd7e480e5e4c2fb8f0b7
dandirayo <72259228+dandirayo@users.noreply.github.com>
2024-06-19 17:59:04 +0700

    19/06/2024
    Pop-Up 3 Box

--- pop_up.js
+++ pop_up.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    // Popup 1
     const missionBox = document.getElementById('mission-box');
     const missionPopup = document.getElementById('mission-popup');
-    const closeBtn = document.querySelector('.close-btn');
+    const closeBtn1 = missionPopup.querySelector('.close-btn');
 
     missionBox.addEventListener('click', () => {
         missionPopup.style.display = 'block';
     });
 
-    closeBtn.addEventListener('click', () => {
+    closeBtn1.addEventListener('click', () => {
         missionPopup.style.display = 'none';
     });
 
@@ -16,4 +17,44 @@ document.addEventListener('DOMContentLoaded', (event) => {
             missionPopup.style.display = 'none';
         }
     });
+
+    // Popup 2
+    const missionBox2 = document.getElementById('mission-box2');
+    const missionPopup2 = document.getElementById('mission-popup2');
+    const closeBtn2 = missionPopup2.querySelector('.close-btn');
+
+    missionBox2.addEventListener('click', () => {
+        missionPopup2.style.display = 'block';
+    });
+
+    closeBtn2.addEventListener('click', () => {
+        missionPopup2.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === missionPopup2) {
+            missionPopup2.style.display = 'none';
+        }
+    });
+
+
+    // Popup 3
+    const missionBox3 = document.getElementById('mission-box3');
+    const missionPopup3 = document.getElementById('mission-popup3');
+    const closeBtn3 = missionPopup3.querySelector('.close-btn');
+
+    missionBox3.addEventListener('click', () => {
+        missionPopup3.style.display = 'block';
+    });
+
+    closeBtn3.addEventListener('click', () => {
+        missionPopup3.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === missionPopup3) {
+            missionPopup3.style.display = 'none';
+        }
+    });
+    
 });
--- scripts.js
+++ scripts.js
@@ -8,23 +8,3 @@ function playVideo(button) {
     video.play();
     video.volume = 1; // Atur volume video
 }
-document.addEventListener('DOMContentLoaded', (event) => {
-    const missionBox = document.getElementById('mission-box');
-    const missionPopup = document.getElementById('mission-popup');
-    const closeBtn = document.querySelector('.close-btn');
-
-    missionBox.addEventListener('click', () => {
-        missionPopup.style.display = 'block';
-    });
-
-    closeBtn.addEventListener('click', () => {
-        missionPopup.style.display = 'none';
-    });
-
-    window.addEventListener('click', (event) => {
-        if (event.target === missionPopup) {
-            missionPopup.style.display = 'none';
-        }
-    });
-});
-
