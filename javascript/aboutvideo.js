36dd05d8f6b701026dfb4ed25f4fdc7b62d4af90
dandirayo <72259228+dandirayo@users.noreply.github.com>
2024-12-27 11:01:35 +0700

    12/27/2024 V2

---
+++ javascript/aboutvideo.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const videoElement = document.getElementById('aboutVideo');
+    const playThumbnail = document.getElementById('playThumbnail');
+
+    // Fungsi untuk memulai video
+    function playVideo() {
+        videoElement.play(); // Memulai video
+        playThumbnail.style.display = 'none'; // Sembunyikan thumbnail
+    }
+
+    // Event listener untuk thumbnail
+    playThumbnail.addEventListener('click', playVideo);
+
+    // Event listener untuk mengembalikan thumbnail jika video selesai
+    videoElement.addEventListener('ended', () => {
+        playThumbnail.style.display = 'flex'; // Tampilkan kembali thumbnail
+    });
+});
